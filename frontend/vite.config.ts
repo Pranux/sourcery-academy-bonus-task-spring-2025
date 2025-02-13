2a7bd95eda5c35dc784f6ea6721351bbea90aa9e
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2025-02-13 14:30:53 +0200

    Made frontend container run without errors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+      host: "0.0.0.0",
+      port: 3000,
   },
   plugins: [
       react(),
